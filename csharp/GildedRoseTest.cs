2ce096b23536ca31124449114acc9e3f50566116
Philip Devine <Devine33@live.co.uk>
2020-11-07 16:45:30 +0000

    Added some initial tests

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,18 +1,59 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp
 {
     [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
+        private GildedRose _gildedRose;
+        private IList<Item> _itemList;
+
+        [SetUp]
+        public void Setup()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            _itemList = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            };
+            _gildedRose = new GildedRose(_itemList);
+        }
+
+        [Test]
+        public void SulfurasLegendaryDosentLoseValue()
+        {
+            var hand = _itemList.First(x => x.Name == "Sulfuras, Hand of Ragnaros");
+            _gildedRose.UpdateQuality();
+
+            Assert.AreEqual(80, hand.Quality);
+            Assert.AreEqual(0, hand.SellIn);
+        }
+
+        [Test]
+        public void AgedBrieQualityImprovesAndLosesValue()
+        {
+            var brie = _itemList.First(x => x.Name == "Aged Brie");
+            _gildedRose.UpdateQuality();
+
+
+            Assert.AreEqual(1, brie.Quality);
+            Assert.AreEqual(1, brie.SellIn);
+        }
+
+        [Test]
+        public void AgedBrieQualityDoublesWhenSellInIsZero()
+        {
+            var brie = _itemList.First(x => x.Name == "Aged Brie");
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+
+            Assert.AreEqual(4, brie.Quality);
+            Assert.AreEqual(-1, brie.SellIn);
         }
     }
 }
