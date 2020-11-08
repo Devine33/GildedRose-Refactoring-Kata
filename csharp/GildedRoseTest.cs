39282077de0e23c724a851f48cd712fb2e75b0cf
Philip Devine <Devine33@live.co.uk>
2020-11-08 15:03:51 +0000

    Added some more tests for existing functionality

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -9,15 +9,21 @@ namespace csharp
     {
         private GildedRose _gildedRose;
         private IList<Item> _itemList;
-
+        private const string Vest = "+5 Dexterity Vest";
+        private const string Brie = "Aged Brie";
+        private const string Elixir = "Elixir of the Mongoose";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
         [SetUp]
         public void Setup()
         {
             _itemList = new List<Item>
             {
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 0},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 0, Quality = 7},
                 new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
             };
             _gildedRose = new GildedRose(_itemList);
@@ -26,7 +32,7 @@ namespace csharp
         [Test]
         public void SulfurasLegendaryDosentLoseValue()
         {
-            var hand = _itemList.First(x => x.Name == "Sulfuras, Hand of Ragnaros");
+            var hand = _itemList.First(x => x.Name == Sulfuras);
             _gildedRose.UpdateQuality();
 
             Assert.AreEqual(80, hand.Quality);
@@ -34,9 +40,19 @@ namespace csharp
         }
 
         [Test]
+        public void ItemQualityDegradesTwiceAsFast()
+        {
+            var elixir = _itemList.First(x => x.Name == Elixir && x.SellIn == 0);
+            _gildedRose.UpdateQuality();
+
+            Assert.AreEqual(5, elixir.Quality);
+            Assert.AreEqual(-1, elixir.SellIn);
+        }
+
+        [Test]
         public void AgedBrieQualityImprovesAndLosesValue()
         {
-            var brie = _itemList.First(x => x.Name == "Aged Brie");
+            var brie = _itemList.First(x => x.Name == Brie);
             _gildedRose.UpdateQuality();
 
 
@@ -47,7 +63,7 @@ namespace csharp
         [Test]
         public void AgedBrieQualityDoublesWhenSellInIsZero()
         {
-            var brie = _itemList.First(x => x.Name == "Aged Brie");
+            var brie = _itemList.First(x => x.Name == Brie);
             _gildedRose.UpdateQuality();
             _gildedRose.UpdateQuality();
             _gildedRose.UpdateQuality();
@@ -55,5 +71,38 @@ namespace csharp
             Assert.AreEqual(4, brie.Quality);
             Assert.AreEqual(-1, brie.SellIn);
         }
+
+        [Test]
+        public void QualityIsNeverNegative()
+        {
+            var vest = _itemList.First(x => x.Name == Vest && x.Quality == 0);
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+
+            Assert.LessOrEqual(-1, vest.Quality);
+        }
+
+
+        [Test]
+        public void BackstagePassQualityIncreasesByTwoWhen10DaysOrLess()
+        {
+            var vest = _itemList.First(x => x.Name ==  && x.Quality == 0);
+        }
+
+        [Test]
+        public void BackstagePassQualityIncreasesByThreeWhen5DaysOrLess()
+        {
+            var vest = _itemList.First(x => x.Name == Vest && x.Quality == 0);
+        }
+
+        [Test]
+        public void BackstagePassQualityDropsToZeroAfterConcert()
+        {
+            var vest = _itemList.First(x => x.Name == Vest && x.Quality == 0);
+        }
+
+
+
     }
 }
